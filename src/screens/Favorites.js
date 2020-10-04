2dac9dac18249d297bdd3f3103a3b4303e7a9280
gabrielfr2001 <gabrielfroskowski2001@gmail.com>
2020-10-04 10:38:13 -0300

    Final

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -9,15 +9,17 @@ function FavoritesScreen({ navigation, route }) {
       style={{
         flex: 1,
         backgroundColor: "#fff",
-      }}>    
-      <ScrollView style={{ flex: 1, marginRight: 10, marginLeft: 10 }}>
+      }}
+    >
+      <ScrollView style={{ flex: 1, marginRight: 10, marginLeft: 10, paddingTop:25 }}>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Nikolai Kardashev"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="NK" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 60, marginHorizontal: 24 }}>Yoga</Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -27,15 +29,15 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Carl Gauss"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="CG" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 65, marginHorizontal: 24 }}>Zumba</Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -45,15 +47,17 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Leonhard Euler"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="LE" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 100, marginHorizontal: 24 }}>
+                  Fitness trainer
+                </Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -63,15 +67,15 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Schroedinger"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="S" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 65, marginHorizontal: 24 }}>Boxing</Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -81,15 +85,15 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Bill Gates"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="BG" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 65, marginHorizontal: 24 }}>Tai Chi</Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -99,15 +103,17 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Steve Jobs"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="SJ" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 80, marginHorizontal: 24 }}>
+                  Aerobics
+                </Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -117,15 +123,17 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
         <Card style={{ paddingVertical: 10 }}>
           <Card.Title
-            title="Aloha"
+            title="Mark Zuckerberg"
             subtitle="Your Favorite Trainer"
-            left={() => <Avatar.Text size={48} label="AZ" />}
+            left={() => <Avatar.Text size={48} label="MZ" />}
             right={() => (
               <Card.Actions>
+                <Chip style={{ width: 120, marginHorizontal: 24 }}>
+                  Strength Training
+                </Chip>
                 <Button
                   icon="calendar"
                   // onPress={() => navigation.navigate("Schedule")}
@@ -135,7 +143,6 @@ function FavoritesScreen({ navigation, route }) {
               </Card.Actions>
             )}
           />
-          <Chip style={{ width: 65, marginHorizontal: 24 }}>Yoga</Chip>
         </Card>
       </ScrollView>
     </SafeAreaView>
